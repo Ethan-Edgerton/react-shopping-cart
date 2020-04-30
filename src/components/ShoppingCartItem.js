e8f6a4d1b43a2bfb26a038d7c339dbdfed24a345
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-04-30 18:03:27 -0500

    MVP met

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
-const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
-
-
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+const Item = (props) => {
+  const { cart } = useContext(CartContext);
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.image} alt={`${props.title} book`} />
+      <div>
+        <h1>{props.title}</h1>
+        <p>$ {props.price}</p>
+        <button>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
-
 export default Item;
